6f4010248ce78d26e06fd36307256c0df5bbaa61
gergolokos <lokosgergo@icloud.com>
2026-05-29 16:56:36 +0200

    feature: CompletableFuture

---
+++ concurrency/src/main/java/hu/lokosgergo/completable/CompletableFutureMain.java
@@ -0,0 +1,31 @@
+package hu.lokosgergo.completable;
+
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+
+import java.time.Duration;
+import java.util.concurrent.CompletableFuture;
+
+@Slf4j
+public class CompletableFutureMain {
+
+
+    @SneakyThrows
+    static void main() {
+        //Callable lehetne és akkor lenne a CompletableFuture-nek visszatérési értéke.
+        Runnable task = () -> {
+            try {
+                Thread.sleep(Duration.ofSeconds(2));
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            log.debug("Task finished");
+        };
+        var result = CompletableFuture.runAsync(task)
+                .thenAccept(v -> log.debug("Task2 finished"));
+        //Bevárja a task-ot a mian thread
+        result.get();
+        log.debug("Main finished");
+    }
+
+}
